d5f813159afc7917c6052c6e68b62d25bdedc4d0
Adhe Rama F <adheramaf7@gmail.com>
2023-11-12 07:19:01 +0700

    portfolio images

--- lib/data.ts
+++ lib/data.ts
@@ -1,9 +1,9 @@
 import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaRegCalendarCheck, FaRegCalendarTimes } from "react-icons/fa";
-import Career1 from "@/public/career1.png";
-import Career2 from "@/public/career2.png";
-import Career3 from "@/public/career3.png";
+import CoverCareer from "@/public/cover-career.png";
+import CoverTetra from "@/public/cover-tetra.png";
+import CoverPos from "@/public/cover-pos.png";
 
 export const links = [
   {
@@ -33,14 +33,16 @@ export const links = [
 ] as const;
 
 export const experiencesData = [
+
   {
-    title: "Programmer",
-    location: "PT Renjana Abi Yasa, Indonesia",
+    title: "Programmer Specialist",
+    location: "Tancorp, Indonesia",
     description:
-      "Developed web applications using .NET, Javascript, jQuery, and HTML/CSS",
-    icon: React.createElement(FaRegCalendarTimes),
-    date: "Aug - Oct 2018",
+      "Developed web applications using PHP, Laravel, Javascript, jQuery, React JS, and HTML/CSS. Also developed mobile applications using Dart, and Flutter.",
+    icon: React.createElement(FaRegCalendarCheck),
+    date: "Jul 2021 - present",
   },
+
   {
     title: "Programmer Staff",
     location: "Dinamika University, Indonesia",
@@ -50,12 +52,12 @@ export const experiencesData = [
     date: "Nov 2018 - Jul 2021",
   },
   {
-    title: "Programmer Specialist",
-    location: "Tancorp, Indonesia",
+    title: "Programmer",
+    location: "PT Renjana Abi Yasa, Indonesia",
     description:
-      "Developed web applications using PHP, Laravel, Javascript, jQuery, React JS, and HTML/CSS. Also developed mobile applications using Dart, and Flutter.",
-    icon: React.createElement(FaRegCalendarCheck),
-    date: "Jul 2021 - present",
+      "Developed web applications using .NET, Javascript, jQuery, and HTML/CSS",
+    icon: React.createElement(FaRegCalendarTimes),
+    date: "Aug - Oct 2018",
   },
 ] as const;
 
@@ -63,30 +65,23 @@ export const projectsData = [
   {
     title: "Recruitment Management System",
     description:
-      "The system for managing recruitment process from request until sorting candidates.",
+      "Recruitment management process from request to sorting candidates.",
     tags: ["PHP", "Laravel", "Livewire", 'JavaScript', 'MySQL'],
-    imageUrl: Career1,
+    imageUrl: CoverCareer,
   },
   {
     title: "E-Learning (TETRA)",
     description:
-      "E-Learning for internal Tancorp Company Needed.",
+      "E-Learning platform for employee development.",
     tags: ["PHP", "Laravel", "Livewire", 'JavaScript', 'MySQL'],
-    imageUrl: Career2,
+    imageUrl: CoverTetra,
   },
   {
-    title: "Employee Self Service",
+    title: "POS & Web Dashboard",
     description:
-      "A mobile for employee to submit permits, annual leaves, and recording attendance. Already used in five companies under Tancorp.",
-    tags: ["Dart", "Flutter",],
-    imageUrl: Career3,
-  },
-  {
-    title: "Central Kitchen & POS",
-    description:
-      "Central Kitchen system for managing multiple restaurants centrally alongside with Point of Sales System for transaction on outlet.",
+      "Mobile App POS and Web Dashboard.",
     tags: ["Dart", "Flutter", 'PHP', 'Laravel', 'REST API'],
-    imageUrl: Career3,
+    imageUrl: CoverPos,
   },
 ] as const;
 
